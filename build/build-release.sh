35d6fe7138aec1769be8af1bb60a76e2fc01feac
Imran Pochi <imran@kinvolk.io>
2019-12-04 14:56:49 +0530

    Update golang image to 1.13.0 for building bootkube in a container.
    Signed-off-by: Imran Pochi <imran@kinvolk.io>

--- build/build-release.sh
+++ build/build-release.sh
@@ -5,5 +5,5 @@ BOOTKUBE_ROOT=$(git rev-parse --show-toplevel)
 sudo rkt run \
     --volume bk,kind=host,source=${BOOTKUBE_ROOT} \
     --mount volume=bk,target=/go/src/github.com/kubernetes-sigs/bootkube \
-    --insecure-options=image docker://golang:1.11.1 --exec /bin/bash -- -c \
+    --insecure-options=image docker://golang:1.13.0 --exec /bin/bash -- -c \
     "cd /go/src/github.com/kubernetes-sigs/bootkube && make release"
