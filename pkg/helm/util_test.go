kinvolk/bootkube

commit d290a286063384fb247a5a8053cd9a45e7b20425

pkg/helm: fix missing newline in Helm logs
To improve logs readability.

Signed-off-by: Mateusz Gozdek <mateusz@kinvolk.io>

pkg/helm/util.go
@@ -6,6 +6,8 @@ import (
 	"path/filepath"
 
 	"sigs.k8s.io/yaml"
+
+	"github.com/kubernetes-sigs/bootkube/pkg/util"
 )
 
 // getCharts returns the map structure of charts found in sub directories
@@ -64,4 +66,14 @@ func loadValuesFile(path string) (map[string]interface{}, error) {
 	}
 
 	return values, nil
+}
+
+// log is a wrapper over UserOutput function to be passed to Helm client,
+// as Helm logs comes without newline at the end, which makes the output
+// very difficult to read. This function simply appends the newline at the end
+// of a given format.
+func log(namespace, chartName string) func(format string, a ...interface{}) {
+	return func(format string, a ...interface{}) {
+		util.UserOutput(fmt.Sprintf("%s/%s: %s\n", namespace, chartName, format), a)
+	}
 }

pkg/helm/helm.go
@@ -54,8 +54,8 @@ func InstallCharts(kubeconfigPath string, config clientcmd.ClientConfig, chartsD
 func installChart(kubeconfigPath, namespace, chartName, chartPath string) error {
 	client := kube.GetConfig(kubeconfigPath, "", namespace)
 	actionConfig := &action.Configuration{}
-	if err := actionConfig.Init(client, namespace, defaultHelmStorageDriver, util.UserOutput); err != nil {
-		util.UserOutput(fmt.Sprintf("error initalizing helm --- %v\n", err))
+	if err := actionConfig.Init(client, namespace, defaultHelmStorageDriver, log(namespace, chartName)); err != nil {
+		util.UserOutput(fmt.Sprintf("Error initalizing helm: %v\n", err))
 		return err
 	}
 	util.UserOutput(fmt.Sprintf("Loading chart %q\n", chartName))

commit c5994920ef9e57e34c8dc3cca4906916395222c1

pkg/helm: fix Helm logger formatting
Regression introduced in d290a286063384fb247a5a8053cd9a45e7b20425, where
log helper function did not properly expand given arguments, which
resulted in passing them to util.UserOutput function as a slice instead
of as separate arguments, which caused log messages to be formatted
incorrectly.

This commit fixes it and adds unit test to verify expected behavior.

Closes #14

Signed-off-by: Mateusz Gozdek <mateusz@kinvolk.io>

pkg/helm/util.go
@@ -82,6 +82,6 @@ func loadValuesFile(path string) (map[string]interface{}, error) {
 // of a given format.
 func log(namespace, chartName string) func(format string, a ...interface{}) {
 	return func(format string, a ...interface{}) {
-		util.UserOutput(fmt.Sprintf("%s/%s: %s\n", namespace, chartName, format), a)
+		util.UserOutput(fmt.Sprintf("%s/%s: %s\n", namespace, chartName, format), a...)
 	}
 }

pkg/helm/util_test.go
@@ -1,6 +1,8 @@
 package helm
 
 import (
+	"bytes"
+	"io"
 	"io/ioutil"
 	"os"
 	"path/filepath"
@@ -108,5 +110,46 @@ func TestLoadValuesFile(t *testing.T) {
 	}
 	if data["key"] != "value" {
 		t.Fatal("data not loaded properly")
+	}
+}
+
+// captureStdout is helper function for testing functions, which do not offer specifying where they should write
+// output and write only to os.Stdout. Wrapping such functions using this allows to capture their output as string.
+func captureStdout(printFunction func()) string {
+	// Keep backup of the real stdout.
+	old := os.Stdout
+
+	// Get new pipe to temporarily override os.Stdout so we can capture the output.
+	r, w, _ := os.Pipe()
+	os.Stdout = w
+
+	printFunction()
+
+	outC := make(chan string)
+
+	// Copy the output in a separate goroutine so printing can't block indefinitely.
+	go func() {
+		var buf bytes.Buffer
+		io.Copy(&buf, r)
+		outC <- buf.String()
+	}()
+
+	// Back to normal state.
+	w.Close()
+
+	// Restoring the real stdout.
+	os.Stdout = old
+
+	return <-outC
+}
+
+func TestLogExpandsGivenArguments(t *testing.T) {
+	output := captureStdout(func() {
+		log("foo", "bar")("%d %q", 6, "6")
+	})
+
+	expected := "foo/bar: 6 \"6\"\n"
+	if output != expected {
+		t.Fatalf("unexpected input. Expected %q, got %q", expected, output)
 	}
 }